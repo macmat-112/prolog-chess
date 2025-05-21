cd7fec725c4ac361f51ee7ae25f9f804c376413b
macmat-112 <monod112358@gmail.com>
2025-05-21 13:01:27 +0200

    Update chess.pl

--- chess.pl
+++ chess.pl
@@ -1,14 +1,92 @@
 % turn_w([[wc, sc, gc, hc, kc, gc, sc, wc], [pc, pc, pc, pc, pc, pc, pc, pc], [--, --, --, --, --, --, --, --], [--, --, --, --, --, --, --, --], [--, --, --, --, --, --, --, --], [--, --, --, --, --, --, --, --], [pb, pb, pb, pb, pb, pb, pb, pb], [wb, sb, gb, hb, kb, gb, sb, wb]]).
-turn_w(P):- nl, write('Teraz bia≥y: '), read(A), read(B), read(C), read(D), move(P, A, B, C, D).
-turn_b(P):- nl, write('Teraz czarny: '), read(A), read(B), read(C), read(D), move(P, A, B, C, D).
+turn_w(Board):- nl, write('Teraz bia≈Çy: '), read(A), read(B), read(C), read(D), move_w(Board, A, B, C, D, Result, BoardResult), print_board(BoardResult), next_turn(w, Result, BoardResult).
+turn_b(Board):- nl, write('Teraz czarny: '), read(A), read(B), read(C), read(D), move_b(Board, A, B, C, D, Result, BoardResult), print_board(BoardResult), next_turn(b, Result, BoardResult).
 
-/*
-test(A, B, P):- idz_y(A, 1, B, P).
-idz_y(X, Y, Y, [H|_]):- idz_x(1, X, H), !.
-idz_y(X, Y, Z, [_|T]):- Y<Z, Y1 is Y+1, idz_y(X, Y1, Z, T).
-idz_x(X, X, [H|_]):- write(H), !.
-idz_x(X, Z, [_|T]):- X<Z, X1 is X+1, idz_x(X1, Z, T).
-*/
+move_w(Board, Row1, Col1, Row2, Col2, Result, BoardResult):-
+	nth0(Row1, Board, Row1List),
+	nth0(Col1, Row1List, PieceFrom),
+	nth0(Row2, Board, Row2List),
+	nth0(Col2, Row2List, PieceTo),
+	decide_piece_w(Board, Row1, Row2, Row1List, Row2List, Col1, Col2, PieceFrom, PieceTo, Result, BoardResult).
+
+move_b(Board, Row1, Col1, Row2, Col2, Result, BoardResult):-
+	nth0(Row1, Board, Row1List),
+	nth0(Col1, Row1List, PieceFrom),
+	nth0(Row2, Board, Row2List),
+	nth0(Col2, Row2List, PieceTo),
+	decide_piece_b(Board, Row1, Row2, Row1List, Row2List, Col1, Col2, PieceFrom, PieceTo, Result, BoardResult).
+
+decide_piece_w(Board, Row1, Row2, Row1List, _, Col1, Col2, 'pb', '--', Result, BoardResult):-
+	Row2 is Row1-2,
+	Col2 is Col1,
+	nth0(6, Board, Row1List),
+	check_empty(Board, Row1, Row2, Col1, Col2),
+	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult),
+	Result=0.
+
+decide_piece_w(Board, Row1, Row2, _, _, Col1, Col2, 'pb', '--', Result, BoardResult):-
+	Row2 is Row1-1,
+	Col2 is Col1,
+	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult),
+	Result=1.
+
+decide_piece_w(Board, Row1, Row2, _, Row2List, Col1, Col2, 'pb', PieceTo, Result, BoardResult):-
+	Row2 is Row1-1,
+	Col2 is Col1-1,
+	PieceTo is not '--',
+	destroy_and_swap(Board, Row1, Col1, Row2, Col2, Row2List, BoardResult),
+	Result=1.
+
+decide_piece_w(Board, Row1, Row2, _, Row2List, Col1, Col2, 'pb', PieceTo, Result, BoardResult):-
+	Row2 is Row1-1,
+	Col2 is Col1+1,
+	PieceTo is not '--',
+	destroy_and_swap(Board, Row1, Col1, Row2, Col2, Row2List, BoardResult),
+	Result=1.
+
+decide_piece_b(Board, Row1, Row2, Row1List, _, Col1, Col2, 'pc', '--', Result, BoardResult):-
+	Row2 is Row1+2,
+	Col2 is Col1,
+	nth0(1, Board, Row1List),
+	check_empty(Board, Row1, Row2, Col1, Col2),
+	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult)
+	Result=1.
+
+decide_piece_b(Board, Row1, Row2, _, _, Col1, Col2, 'pc', '--', Result, BoardResult):-
+	Row2 is Row1+1,
+	Col2 is Col1,
+	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult),
+	Result=1.
+
+decide_piece_b(Board, Row1, Row2, _, Row2List, Col1, Col2, 'pc', PieceTo, Result, BoardResult):-
+	Row2 is Row1+1,
+	Col2 is Col1-1,
+	PieceTo is not '--',
+	destroy_and_swap(Board, Row1, Col1, Row2, Col2, Row2List, BoardResult),
+	Result=1.
+
+decide_piece_b(Board, Row1, Row2, _, Row2List, Col1, Col2, 'pc', PieceTo, Result, BoardResult):-
+	Row2 is Row1+1,
+	Col2 is Col1+1,
+	PieceTo is not '--',
+	destroy_and_swap(Board, Row1, Col1, Row2, Col2, Row2List, BoardResult),
+	Result=1.
+
+decide_piece(Board, _, _, _, _, _, _, _, _, Result, BoardResult):-
+	nl,
+	write('Niepoprawny ruch!'),
+	nl,
+	Result=0,
+	BoardResult is Board.
+
+destroy_and_swap(Board, Row1, Col1, Row2, Col2, Row2List, BoardResult):-
+	replace(Row2List, Col2, _, '--', NewRow2List),
+	replace(Board, Row2, _, NewRow2List, NewBoard),
+	swap_pieces(NewBoard, Row1, Col1, Row2, Col2, BoardResult).
+
+replace(List, Index, OldElem, NewElem, NewList):-
+	nth0(Index,List,OldElem,Transfer),
+	nth0(Index,NewList,NewElem,Transfer).
 
 swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult) :-
     nth0(Row1, Board, Row1Get),
@@ -27,101 +105,82 @@ replace_in_row(Row, Col, Piece, ResultRow):-
     nth0(Col, Row, _, Rest),
     nth0(Col, ResultRow, Piece, Rest).
 
-move(Board, Row1, Col1, Row2, Col2):-
-	nth0(Row1, Board, Row1Get),
-	nth0(Col1, Row1Get, Piece),
-	decide_piece(Board, Row1, Row2, Row1Get, Col1, Col2, Piece).
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1 is Row2,
+	Col1>Col2,
+	NewCol1 is Col1-1,
+	nth0(Row1, Board, Row1List),
+	nth0(NewCol1, Row1List, '--'),
+	check_empty(Board, Row1, Row2, NewCol1, Col2).
 
-decide_piece(Board, Row1, Row2, Row1Get, Col1, Col2, 'pb'):-
-	Row2 is Row1-2,
-	nth0(6, Board, Row1Get),
-	check_empty_col_up(Board, Row1, Row2, Col1),
-	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult),
-	print_board(BoardResult),
-	turn_b(BoardResult).
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1 is Row2,
+	Col1<Col2,
+	NewCol1 is Col1+1,
+	nth0(Row1, Board, Row1List),
+	nth0(NewCol1, Row1List, '--'),
+	check_empty(Board, Row1, Row2, NewCol1, Col2).
 
-decide_piece(Board, Row1, Row2, _, Col1, Col2, 'pb'):-
-	Row2 is Row1-1,
-	check_empty_col_up(Board, Row1, Row2, Col1),
-	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult),
-	print_board(BoardResult),
-	turn_b(BoardResult).
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1>Row2,
+	Col1 is Col2,
+	NewRow1 is Row1-1,
+	nth0(NewRow1, Board, NewRow1List),
+	nth0(Col1, NewRow1List, '--'),
+	check_empty(Board, NewRow1, Row2, Col1, Col2).
 
-decide_piece(Board, Row1, Row2, Row1Get, Col1, Col2, 'pc'):-
-	Row2 is Row1+2,
-	nth0(1, Board, Row1Get),
-	check_empty_col_down(Board, Row1, Row2, Col1),
-	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult),
-	print_board(BoardResult),
-	turn_w(BoardResult).
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1<Row2,
+	Col1 is Col2,
+	NewRow1 is Row1+1,
+	nth0(NewRow1, Board, NewRow1List),
+	nth0(Col1, NewRow1List, '--'),
+	check_empty(Board, NewRow1, Row2, Col1, Col2).
 
-decide_piece(Board, Row1, Row2, _, Col1, Col2, 'pc'):-
-	Row2 is Row1+1,
-	check_empty_col_down(Board, Row1, Row2, Col1),
-	swap_pieces(Board, Row1, Col1, Row2, Col2, BoardResult),
-	print_board(BoardResult),
-	turn_w(BoardResult).
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1>Row2,
+	Col1>Col2,
+	NewRow1 is Row1-1,
+	NewCol1 is Col1-1,
+	nth0(NewRow1, Board, NewRow1List),
+	nth0(NewCol1, NewRow1List, '--'),
+	check_empty(Board, NewRow1, Row2, Col1, Col2).
 
-decide_piece(Board, _, _, _, _, _, Piece):-
-	write('Niepoprawny ruch!'),
-	nl,
-	print_board(Board),
-	repeat_move(Piece, Board).
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1<Row2,
+	Col1>Col2,
+	NewRow1 is Row1+1,
+	NewCol1 is Col1-1,
+	nth0(NewRow1, Board, NewRow1List),
+	nth0(NewCol1, NewRow1List, '--'),
+	check_empty(Board, NewRow1, Row2, Col1, Col2).
 
-repeat_move('pb', Board):- turn_w(Board).
-repeat_move('pc', Board):- turn_b(Board).
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1>Row2,
+	Col1<Col2,
+	NewRow1 is Row1-1,
+	NewCol1 is Col1+1,
+	nth0(NewRow1, Board, NewRow1List),
+	nth0(NewCol1, NewRow1List, '--'),
+	check_empty(Board, NewRow1, Row2, Col1, Col2).
 
-check_empty_col_up(_, Row, Lim, _):- Row is Lim.
+check_empty(Board, Row1, Row2, Col1, Col2):-
+	Row1<Row2,
+	Col1<Col2,
+	NewRow1 is Row1+1,
+	NewCol1 is Col1+1,
+	nth0(NewRow1, Board, NewRow1List),
+	nth0(NewCol1, NewRow1List, '--'),
+	check_empty(Board, NewRow1, Row2, Col1, Col2).
 
-check_empty_col_up(Board, Row, Lim, Col):-
-	Row>Lim,
-	NextRow is Row-1,
-	nth0(NextRow, Board, NextRowGet),
-	nth0(Col, NextRowGet, '--'),
-	check_empty_col_up(Board, NextRow, Lim, Col).
-
-check_empty_col_down(_, Row, Lim, _):- Row is Lim.
-
-check_empty_col_down(Board, Row, Lim, Col):-
-	Row<Lim,
-	NextRow is Row+1,
-	nth0(NextRow, Board, NextRowGet),
-	nth0(Col, NextRowGet, '--'),
-	check_empty_col_up(Board, NextRow, Lim, Col).
-
-go_to(Board, Row1, Col1):-
-	nth0(Row1, Board, Row1Get),
-	nth0(Col1, Row1Get, _).
+check_empty(_, Row1, Row2, Col1, Col2):-
+	Row1 is Row2,
+	Col1 is Col2.
 
 print_board([]).
 print_board([H|T]):- write(H), nl, print_board(T).
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-print_2d(A, B, P):- goto_y(A, 1, B, P).
-goto_y(X, Y, Y, [H|_]):- goto_x(1, X, H), !.
-goto_y(X, Y, Z, [_|T]):- Y<Z, Y1 is Y+1, goto_y(X, Y1, Z, T).
-goto_x(X, X, [H|_]):- write(H), !.
-goto_x(X, Z, [_,T]):- X<Z, X1 is X+1, goto_x(X1, Z, T).
-
-?- swap_elements([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 0, 1, 2, 2, Result).
-Result = [[1, 9, 3], [4, 5, 6], [7, 8, 2]].
-*/
+next_turn(w, 1, Board):- turn_b(Board).
+next_turn(w, 0, Board):- turn_w(Board).
+next_turn(b, 1, Board):- turn_w(Board).
+next_turn(b, 0, Board):- turn_b(Board).
